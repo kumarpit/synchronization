275a52a1e56aa6430d9dc124b93643b4fa322e6c
UBC Student <krarpit7@gambier.students.cs.ubc.ca>
2022-09-15 10:11:55 -0700

    macros

--- classics/producer_consumer.c
+++ classics/producer_consumer.c
@@ -1,23 +1,26 @@
 #include<stdio.h>
 #include<semaphore.h>
 #include<pthread.h>
+#include<unistd.h>
+
+#define BUFFER_SIZE 10
+#define NUM_IT 20
 
 sem_t mutex, items, spaces;
 
-int buffer_size = 10;
-int buffer[10];
+int buffer[BUFFER_SIZE];
 int current_index = 0;
 
 void *producer(void *a) {
     int event;
-    for (int i=0; i < 20; i++) {
+    for (int i=0; i < NUM_IT; i++) {
         event = i;
         
         sem_wait(&spaces);
         
         sem_wait(&mutex);
             buffer[current_index] = event;
-            // printf("produced: %d at index: %d\n", buffer[current_index], current_index);
+            printf("produced: %d at index: %d\n", buffer[current_index], current_index);
             current_index++;
         sem_post(&mutex);
         
@@ -26,12 +29,13 @@ void *producer(void *a) {
 }
 
 void *consumer(void *a) {
-    for (int i=0; i < 20; i++) {
+    for (int i=0; i < NUM_IT; i++) {
+        sleep(1);
         sem_wait(&items);
 
         sem_wait(&mutex);
             current_index--;
-            printf("consumed: %d at index: %d \n", buffer[current_index], current_index);
+            // printf("consumed: %d at index: %d \n", buffer[current_index], current_index);
         sem_post(&mutex);
 
         sem_post(&spaces);
@@ -41,7 +45,7 @@ void *consumer(void *a) {
 int main() {
     pthread_t t1, t2;
 
-    sem_init(&spaces, 0, buffer_size);
+    sem_init(&spaces, 0, BUFFER_SIZE);
     sem_init(&mutex, 0, 1);
     sem_init(&items, 0, 0);
 
